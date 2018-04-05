96283d92478cbeaddd1e78fa22eaa4474bb19d59
oeuvrayjoris <oeuvray.joris@gmail.com>
2018-04-05 10:20:38 +0200

    Page d'accueil

---
+++ front/src/components/views/accueil.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import '../stylesheets/style.scss';
+import { Button } from 'react-bootstrap';
+
+class Accueil extends Component {
+  render() {
+    return (
+      <h1>Accueil</h1>
+    );
+  }
+}
+
+export default App;
