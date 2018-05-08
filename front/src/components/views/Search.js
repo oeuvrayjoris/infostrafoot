d9b07c320f0a3e844d9987092b6e830897af6cd2
Davdouv <d4vid.n@hotmail.fr>
2018-05-08 19:08:03 +0200

    Search page needs to be fixed

--- front/src/components/views/Search.js
+++ front/src/components/views/Search.js
@@ -3,15 +3,42 @@ import '../../styles/sass/style.scss';
 import Menu from '../Menu.js';
 import Header from '../Header.js';
 
+import Player from '../Player'
+import ItemTypes from '../ItemTypes'
+import ApiService from '../ApiService'
+
+const Api = new ApiService();
 
 class Search extends Component {  
 
   constructor(props) {
     super(props)
     this.state = {
-      search: ''
+      search: '',
+      players: []
     }
+
+    this.setPlayers = this.setPlayers.bind(this)
   }
+
+  componentDidMount() {
+    Api.getPlayers().then(result => this.setPlayers(result))
+  }
+
+  setPlayers(res) {
+    const players = res.map(player => ({
+        id: player.id,
+        firstname: player.firstname,
+        lastname: player.lastname,
+        username: player.username,
+        display: 'block',
+        type: ItemTypes.BOX
+    }))
+
+    this.setState({
+        players : players
+    })
+}
 
   // Handle the changed values on the form
   handleChange = e => {
@@ -28,6 +55,9 @@ class Search extends Component {
   }
 
   render() {
+
+    const players = this.state.players
+
     return (
 
       <div className="row" id="main" style={{ height: window.innerHeight}}>
@@ -52,7 +82,22 @@ class Search extends Component {
                     </span>
                 </form>
               </div>
-            <div className="col-md-9"></div>
+              <div className="row">
+                {players.map(({ id, firstname, lastname, username, display }, index) =>  {
+                  /*
+                  return (
+                    <Player
+                        id={id}
+                        firstname={firstname}
+                        lastname={lastname}
+                        username={username}
+                        display={display}
+                        key={id}
+                    />
+                  )
+                  */
+                })}
+              </div>
             </div>
           </div>
         </div>
