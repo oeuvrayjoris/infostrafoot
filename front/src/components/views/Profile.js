ab1add441ea6c321ed8b8a774db3cfcf8550c7db
Davdouv <d4vid.n@hotmail.fr>
2018-05-16 23:19:27 +0200

    Match stat not finished yet

--- front/src/components/views/Profile.js
+++ front/src/components/views/Profile.js
@@ -19,7 +19,7 @@ class Profile extends Component {
     super(props)
     this.state = {
       infos_player : {
-        id: 1,
+        id: 38,
         mail: '',
         username: '',
         password: '',
@@ -28,7 +28,8 @@ class Profile extends Component {
         birthdate: ''
       },
       victory_stat : null,
-      best_role : ""
+      best_role : null,
+      match_stat : null,
     }
     this.setMyProfil = this.setMyProfil.bind(this)
   }
@@ -51,7 +52,8 @@ class Profile extends Component {
       this.setState({
         infos_player : this.getProfilInfos(profil),
         victory_stat : this.getVictoryStat(stats),
-        best_role : this.getBestRole(stats)
+        best_role : this.getBestRole(stats),
+        match_stat : this.getGoals(stats)
       })
     })
   }
@@ -102,6 +104,125 @@ class Profile extends Component {
     )
   }
 
+  getGoals(stats) {
+    // On récupére le dernier match joué, ses goals et ses équipes
+    const match = stats.last_match
+    const goals = match.goals
+    const teams = match.teams
+    const myTeamIndex = this.knowMyTeam(teams)
+    //const baseTime = this.getTime(match)
+    const baseTime = {"hour":"17","minutes":"22","seconds":"10"}
+    const goalIndexes = goals.map((goal, index) => (
+      {"team_num":this.getGoalTeam(goal, teams),"index":index,"gamelle":goal.gamelle,"own_goal":goal.own_goal}
+    ))
+    const goalTime = goals.map(goal => this.getGoalTime(goal, baseTime)).map(time => this.timeToString(time))
+    const scores = this.getScores(goalIndexes)
+
+    //const goalHistory = {"index":goalIndexes, "time":goalTime}
+    //const goalTeam1 = goalIndexes.map(goal => {if (goal.team_num === 0) return goal.index})
+    //const goalTeam2 = goalIndexes.map(goal => {if (goal.team_num === 1) return goal.index})
+
+    const dataTeam1 = goalTime.map((time, index) => ({
+      "x":time,
+      "y":scores[index].score_1
+    }))
+    const dataTeam2 = goalTime.map((time, index) => ({
+      "x":time,
+      "y":scores[index].score_2
+    }))
+    console.log(dataTeam1)
+    console.log(dataTeam2)
+    const datas = [
+      {
+        "id" : "Mon équipe",
+        "data" : dataTeam1
+      },
+      {
+        "id" : "Equipe adverse",
+        "data" : dataTeam2
+      }
+    ]
+    return datas
+  }
+
+  // Renvoie l'index de la team dans teams
+  knowMyTeam(teams) {
+    // J'utilise le if else car le ternaire ne fonctionne pas ici, je ne sais pas pourquoi
+    if (teams[0].players[0].id === this.state.infos_player.id || teams[0].players[1].id === this.state.infos_player.id)
+      return 0
+    else return 1
+  }
+
+  // Pour savoir le goal appartient à quelle équipe
+  getGoalTeam(goal, matchTeams) {
+    // Pareil ici le ternaire ne fonctionne pas, je ne sais pas pourquoi
+    if (matchTeams[0].players[0].id === goal.player_id || matchTeams[0].players[1].id === goal.player_id)
+    return 0
+    else return 1
+  }
+
+  // Pour connaitre l'horraire du début du match qui sera la référence
+  getTime(object) {
+    const time = object.created_at.substr(11).split(":", 3);
+    const hour = time[0]
+    const minutes = time[1]
+    const seconds = time[2]
+
+    return {"hour":hour,"minutes":minutes,"seconds":seconds}
+  }
+
+  // Pour connaitre le temps du but dans le match
+  getGoalTime(goal, baseTime) {
+    const goalTime = this.getTime(goal)
+    const _hour = goalTime.hour - baseTime.hour
+    const _minutes = goalTime.minutes - baseTime.minutes
+    const _seconds = goalTime.seconds - baseTime.seconds
+
+    const hour = (_hour < 0) ? _hour+24 : _hour
+    const minutes = (_minutes < 0) ? _minutes+60 : _minutes
+    const seconds = (_seconds < 0) ? _seconds+60 : _seconds
+
+    return {"hour":hour,"minutes":minutes,"seconds":seconds}
+  }
+
+  // Transform time object to string
+  timeToString(time) {
+    const hour = (time.hour < 10) ? "0"+time.hour : time.hour
+    const minutes = (time.minutes < 10) ? "0"+time.minutes : time.minutes
+    const seconds = (time.seconds < 10) ? "0"+time.seconds : time.seconds
+    return minutes+":"+seconds
+    //return hour+":"+minutes+":"+seconds
+  }
+
+  // Renvoie un tableau contenant l'historique des scores
+  getScores(goals) {
+    let score_1 = 0
+    let score_2 = 0
+    const scores = goals.map(goal => ( 
+      (goal.team_num === 0)
+      ? (goal.own_goal)
+        ? score_2++
+        : (goal.gamelle)
+          ? (score_2 === 0)
+            ? score_1++
+            : score_2--
+          : score_1++
+      : (goal.own_goal)
+        ? score_1++
+        : (goal.gamelle)
+          ? (score_1 === 0)
+            ? score_2++
+            : score_1--
+          : score_2++
+          ,
+    {
+      "score_1" : score_1,
+      "score_2" : score_2
+    }))
+    return scores
+  }
+  
+
   render() {
     Auth.login({
       username: 'babou97',
@@ -136,7 +257,7 @@ class Profile extends Component {
                 <div className="section flexbox" id="s2">
                   <div className="flexbox flex-column">
                     {this.state.victory_stat  // NEED TO FIX CSS HERE
-                      ? <h1>{this.getRatio()}<h4>de victoires</h4></h1>
+                      ? <h1>{this.getRatio()} de victoires</h1>
                       : ( <span></span> )
                     }
                   </div>
@@ -156,8 +277,8 @@ class Profile extends Component {
               </div>
               <div className="col-md-9">
                 <div className="section flexbox" id="s2">
-                  {this.state.victory_stat
-                      ? <Line repositories={this.state.victory_stat}/>
+                  {this.state.match_stat
+                      ? <Line repositories={this.state.match_stat}/>
                       : ( <span></span> )
                   }
               </div>
